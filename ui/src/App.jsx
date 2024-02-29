118f7a4b6be5c1ce17b8d428811eaa93c70bbf81
jingjingcai <jcai28@usfca.edu>
2024-02-29 13:59:08 -0800

    setup the grid background

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -23,11 +23,14 @@ function App() {
     <>
     <CssBaseline/>
     <Container maxWidth={false} disableGutters style={{ height: '100vh' }}>
-      <Box sx={{ bgcolor: '#cfe8fc', height: "100%", padding: "20px 12px" }}>
-        <Grid container columnSpacing={1} style={{ height: "100%", width: "100%" }}>
+      <Box sx={{ background: 'rgb(30,125,91)',
+background: 'linear-gradient(0deg, rgba(30,125,91,1) 0%, rgba(21,93,120,1) 100%)',
+height: "100%", padding: "20px 12px" }}>
+        <Grid container columnSpacing={4} style={{height: "100%", width: "100%" }}>
           <Grid item xs={3}>
             <Box height="100%">
-              <Paper style={{ height: '100%'}}>
+              <Paper style={{ background: 'rgba(255, 255, 255, 0.2)', borderRadius: '10px', 
+              boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.4)', marginLeft: "1rem", height: '100%'}}>
                 {/* Your content here */}
                 Left Column
               </Paper>
@@ -35,7 +38,8 @@ function App() {
           </Grid>
           <Grid item xs={9}>
             <Box height="100%">
-              <Paper style={{ height: '100%'}}>
+              <Paper className="paper" style={{ background: 'rgba(255, 255, 255, 0.2)', borderRadius: '10px', 
+              boxShadow: '5px 5px 5px rgba(0, 0, 0, 0.4)',  height: '100%'}}>
                 {/* Your content here */}
                 <Outlet/>
               </Paper>
