0d5aa2c0d12b9036b197cc1d83b3addcbaaae3c7
Seth Villa <sethvilla13@gmail.com>
2024-03-17 15:34:21 -0700

    test backend workflow

---
+++ app/api/test/test_main.py
@@ -0,0 +1,8 @@
+from fastapi.testclient import TestClient
+
+from ..main import app
+
+def test_read_main():
+    response = client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == {"success": "Hello World"}
