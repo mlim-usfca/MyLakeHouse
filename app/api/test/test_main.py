cb4a0d2574c09009ccaeb83ed89019b726530be0
Seth Villa <sethvilla13@gmail.com>
2024-03-17 16:05:41 -0700

    update test, test workflow

--- app/api/test/test_main.py
+++ app/api/test/test_main.py
@@ -2,6 +2,8 @@ from fastapi.testclient import TestClient
 
 from api.main import app
 
+client = TestClient(app)
+
 def test_read_main():
     response = client.get("/test")
     assert response.status_code == 200
