49d853a547d81186a4986dc3d6236d155f5739f4
Seth Villa <sethvilla13@gmail.com>
2024-03-17 19:28:01 -0700

    update FE UI workflow include linting

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/jsx-uses-react': 2,
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -5,16 +5,16 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import axios from "axios"
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 
 export const App = () => {
