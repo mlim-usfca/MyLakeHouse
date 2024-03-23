0c580c50dfc894c3e16fd7d9575723625714d590
Yin Hu (Eva) <yinhueva@gmail.com>
2024-03-22 20:56:50 -0700

    add test for CustomizedListener

---
+++ spark-listeners/src/main/test/CustomizedListenerTest.scala
@@ -0,0 +1,20 @@
+import listeners.CustomizedListener
+import org.apache.spark.sql.SparkSession
+
+object CustomizedListenerTest {
+  def main(args: Array[String]): Unit = {
+    // create a Spark session
+    val sparkSession = SparkSession.builder()
+      .master("local[*]")
+      .config("spark.extraListeners", "listeners.CustomizedListener") // ensures that the listener is instantiated before the Spark context is created
+      .getOrCreate()
+
+    // register the instantiated listener object with the Spark context
+    val customizedListener_1 = new listeners.CustomizedListener
+    sparkSession.sparkContext.addSparkListener(customizedListener_1)
+
+    // create 1 application for Spark to do and for customized listener to log
+    import sparkSession.implicits._
+    (0 to 100).toDF("nr").repartition(30).collect()
+  }
+}
