990e39f01947294b6ebdea0ac79ff57c69ad42ba
agupta2095 <guptaakanksha2196@yahoo.in>
2024-04-12 17:49:31 -0700

    Added logic for getting a table schema

--- app/api/service/table_metadata_service.py
+++ app/api/service/table_metadata_service.py
@@ -11,4 +11,25 @@ class TableMetadata():
         self.catalog = load_catalog('local')
 
     def getTableSchema(self, db_name, table_name):
-        return 200, "Ok"
+        logging.info("In Get Table Schema service")
+        if not db_name or not table_name:
+            return 404, "Ill-formed request: 'table_name', and 'database_name' cannot be empty."
+        try:
+            table = self.spark.table(f'local.{db_name}.{table_name}')
+            schema = table.schema
+            if not schema:
+                return 404, f"Cannot fetch the schema of table {db_name}.{table_name}"
+            else:
+               returnVal = []
+               for field in schema.fields:
+                   fieldVals = {}
+                   jsonObj = field.jsonValue()
+                   for key, val in jsonObj.items():
+                       # metadata field is empty
+                       if key is not "metadata":
+                           fieldVals[key] = val
+                   returnVal.append(fieldVals)
+               return 200, returnVal
+        except Exception as error:
+            logging.info(f"Error in getTableSchema: {error}")
+            return 500, error
