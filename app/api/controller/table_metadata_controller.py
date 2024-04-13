a6df95529266bf68c22b6f02b117f562f78e9d9d
agupta2095 <guptaakanksha2196@yahoo.in>
2024-04-12 19:53:48 -0700

    Added comments and table exist check

--- app/api/controller/table_metadata_controller.py
+++ app/api/controller/table_metadata_controller.py
@@ -19,52 +19,58 @@ class TableMetadataController():
         self.table_metadata_service = table_metadata_service
 
 
-@controller.route.get('/getSchema',
-                      tags=['table-metadata-controller'],
-                      description='Get the schema of the table')
+    """
+               Endpoint to get the table schema.
+    """
+    @controller.route.get('/getSchema',
+                          tags=['table-metadata-controller'],
+                          description='Get the schema of the table')
+    def get_table_schema(self, db_name : str, table_name : str):
+        try:
+            status_code, data = self.table_metadata_service.getTableSchema(db_name, table_name)
+            if status_code == 200:
+                return data
+            else:
+                return HTTPException(status_code=status_code, detail=data)
+        except Exception as error:
+            logging.error("Error: TableMetadataController: /metadata/getSchema:", error)
+            return HTTPException(status_code=500, detail="Internal server error.")
 
 
-def get_table_schema(self, db_name : str, table_name : str):
-    try:
-        status_code, data = self.table_metadata_service.getTableSchema(db_name, table_name)
-        if status_code == 200:
-            return data
-        else:
-            return HTTPException(status_code=status_code, detail=data)
-    except Exception as error:
-        logging.error("Error: TableMetadataController: /metadata/getSchema:", error)
-        return HTTPException(status_code=500, detail="Internal server error.")
+    """
+                Endpoint to get information about all the data files of a table.
+    """
+    @controller.route.get('/getDataFiles',
+                          tags=['table-metadata-controller'],
+                          description='Returns a dictionary of data files multiple properties like location, number of records \
+                                       null value counts for each column etc. By default returns 25 rows, set the offset value to \
+                                       show more records, like pagination')
+    def get_all_datafiles(self, db_name: str, table_name: str, limit: int = 25, offset: int = 0):
+        try:
+            status_code, data = self.table_metadata_service.getDataFiles(db_name, table_name, limit, offset)
+            if status_code == 200:
+                return data
+            else:
+                return HTTPException(status_code=status_code, detail=data)
+        except Exception as error:
+            logging.error("Error: TableMetadatController: /metadata/getDataFiles: ", error)
+            return HTTPException(status_code=500, detail="Internal Server error")
 
 
-@controller.route.get('/getDataFiles',
-                      tags=['table-metadata-controller'],
-                      description='Returns a dictionary of data files multiple properties like location, number of records \
-                                   null value counts for each column etc. By default returns 25 rows, set the offset value to \
-                                   show more records, like pagination')
-def get_all_datafiles(self, db_name: str, table_name: str, limit: int = 25, offset: int = 0):
-    try:
-        status_code, data = self.table_metadata_service.getDataFiles(db_name, table_name, limit, offset)
-        if status_code == 200:
-            return data
-        else:
-            return HTTPException(status_code=status_code, detail=data)
-    except Exception as error:
-        logging.error("Error: TableMetadatController: /metadata/getDataFiles: ", error)
-        return HTTPException(status_code=500, detail="Internal Server error")
-
-
-
-@controller.route.get('/getTableInfo',
-                      tags=['table-metadata-controller'],
-                      description='Returns a dictionary of varioud information about table like its location, current snapshot id, \
-                                  last updated at timestamp, owner etc')
-def get_table_info(self, db_name: str, table_name: str):
-    try:
-        status_code, data = self.table_metadata_service.getTableInfo(db_name, table_name)
-        if status_code == 200:
-            return data
-        else:
-            return HTTPException(status_code=status_code, detail=data)
-    except Exception as error:
-        logging.error("Error: TableMetadataController: /metadata/getTableInfo:", error)
-        return HTTPException(status_code=500, detail="Internal server error.")
+    """
+                Endpoint to get metadata information about a table.
+    """
+    @controller.route.get('/getTableInfo',
+                          tags=['table-metadata-controller'],
+                          description='Returns a dictionary of varioud information about table like its location, current snapshot id, \
+                                      last updated at timestamp, owner etc')
+    def get_table_info(self, db_name: str, table_name: str):
+        try:
+            status_code, data = self.table_metadata_service.getTableInfo(db_name, table_name)
+            if status_code == 200:
+                return data
+            else:
+                return HTTPException(status_code=status_code, detail=data)
+        except Exception as error:
+            logging.error("Error: TableMetadataController: /metadata/getTableInfo:", error)
+            return HTTPException(status_code=500, detail="Internal server error.")
