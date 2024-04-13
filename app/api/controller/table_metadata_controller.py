5cfd71a3ed43af81f7d9a7d853c49c7897265a95
agupta2095 <guptaakanksha2196@yahoo.in>
2024-04-12 18:46:19 -0700

    Added some logic for getting data files of a table

--- app/api/controller/table_metadata_controller.py
+++ app/api/controller/table_metadata_controller.py
@@ -18,6 +18,7 @@ class TableMetadataController():
     def __init__(self, table_metadata_service: TableMetadata = Depends()):
         self.table_metadata_service = table_metadata_service
 
+
 @controller.route.get('/getSchema',
                       tags=['table-metadata-controller'],
                       description='Get the schema of the table')
@@ -33,3 +34,21 @@ def get_table_schema(self, db_name : str, table_name : str):
     except Exception as error:
         logging.error("Error: TableMetadataController: /metadata/getSchema:", error)
         return HTTPException(status_code=500, detail="Internal server error.")
+
+
+@controller.route.get('/getDataFiles',
+                      tags=['table-metadata-controller'],
+                      description='Returns a dictionary of data files multiple properties like location, number of records \
+                                   null value counts for each column etc. By default returns 25 rows, set the offset value to \
+                                   show more records, like pagination')
+def get_all_datafiles(self, db_name: str, table_name: str, limit: int = 25, offset: int = 0):
+    try:
+        status_code, data = self.table_metadata_service.getDataFiles(db_name, table_name, limit, offset)
+        if status_code == 200:
+            return data
+        else:
+            return HTTPException(status_code=status_code, detail=data)
+    except Exception as error:
+        logging.error("Error: TableMetadatController: /metadata/getDataFiles: ", error)
+        return HTTPException(status_code=500, detail="Internal Server error")
+
