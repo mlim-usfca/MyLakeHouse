e8a8a9d1a77d78b9aa36661c5ecd6027314ac960
vinayusfca <109916498+vinayusfca@users.noreply.github.com>
2024-04-30 14:36:48 -0700

    Added scheduler to remove orphan files.

--- app/api/cronjobs/IcebergMaintainence.py
+++ app/api/cronjobs/IcebergMaintainence.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from json import loads
 
@@ -5,29 +6,46 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from fastapi import Depends
 from ..utils.SparkConnection import SparkConnection
-from ..service.dashboard_service import DashboardService
 from datetime import datetime
 
 class IcebergMaintainence(object):
 
-    def __init__(self, scheduler: AsyncIOScheduler, dashboard_service:  DashboardService = Depends()):
+    def __init__(self, scheduler: AsyncIOScheduler):
+        with open("./config.json") as configFile:
+            self.config = json.load(configFile)
         spark_conn_obj = SparkConnection()
         self.spark = spark_conn_obj.get_spark_session()
-        self.dashboard_service = dashboard_service
+        self.catalog = spark_conn_obj.get_catalog()
         self.scheduler = scheduler
         self.add_jobs()
 
     def add_jobs(self):
-        expire_snapshot_trigger = CronTrigger(day='last', hour=23, minute=50)
+        ##### Expire snapshot
+        expire_day, expire_hour, expire_minute = self.get_trigger_parameters("expire_snapshot_trigger")
+        expire_snapshot_trigger = CronTrigger(day=expire_day, hour=expire_hour, minute=expire_minute)
         # expire_snapshot_trigger = CronTrigger(second=20)
         self.scheduler.add_job(self.expire_snapshot, expire_snapshot_trigger)
 
-        compaction_trigger = CronTrigger(day='last', hour=23, minute=55)
+        # ##### Compaction Trigger
+        compact_day, compact_hour, compact_minute= self.get_trigger_parameters("compaction_trigger")
+        compaction_trigger = CronTrigger(day=compact_day, hour=compact_hour, minute=compact_minute)
         # compaction_trigger = CronTrigger(second=40)
         self.scheduler.add_job(self.compaction, compaction_trigger)
 
+        #### Remove Orphan
+        remove_orphan_day, remove_orphan_hour, remove_orphan_minute = self.get_trigger_parameters("remove_orphan_trigger")
+        remove_orphan_trigger = CronTrigger(day=remove_orphan_day, hour=remove_orphan_hour, minute=remove_orphan_minute)
+        # remove_orphan_trigger = CronTrigger(second=55)
+        self.scheduler.add_job(self.remove_orphan, remove_orphan_trigger)
+
+    def get_trigger_parameters(self, trigger):
+        day = self.config["scheduler"][trigger]["day"]
+        hour = self.config["scheduler"][trigger]["hour"]
+        minute = self.config["scheduler"][trigger]["minute"]
+        return day, hour, minute
+
     async def expire_snapshot(self):
-        logging.info("expire_snapshot")
+        logging.info("In expire_snapshot")
         try:
             # Ensure Spark session is available
             if self.spark is None:
@@ -48,7 +66,7 @@ class IcebergMaintainence(object):
                 table_list = [table.name for table in tables]
                 for table_name in table_list:
                     # 1. Expire all snapshot except last 1 for each month.
-                    all_snapshots = self.spark.sql(f'select * from local.{db}.{table_name}.snapshots order by committed_at;')
+                    all_snapshots = self.spark.sql(f'select * from {self.catalog}.{db}.{table_name}.snapshots order by committed_at;')
                     # all_snapshots = self.spark.sql(f'select * from local.{db}.{table_name}.snapshots WHERE committed_at LIKE \'{current_year}-{current_month}%\' order by committed_at;')
                     # Filter snapshots for the specified month
                     snapshot_ids = []
@@ -61,7 +79,7 @@ class IcebergMaintainence(object):
                     if len(snapshot_ids) > 1:
                         # Construct the array string representation
                         snapshot_ids_str = ','.join(str(id) for id in snapshot_ids[0:-1])
-                        expire_snapshot_query = f'CALL local.system.expire_snapshots(table => \'{db}.{table_name}\', snapshot_ids => ARRAY({snapshot_ids_str}))'
+                        expire_snapshot_query = f'CALL {self.catalog}.system.expire_snapshots(table => \'{db}.{table_name}\', snapshot_ids => ARRAY({snapshot_ids_str}))'
                         self.spark.sql(expire_snapshot_query)
                 logging.info("Executed expire_snapshot query.")
         except Exception as error:
@@ -79,8 +97,26 @@ class IcebergMaintainence(object):
                 table_list = [table.name for table in tables]
                 for table_name in table_list:
                     # 2. Just call Rewrite_data_files  query on the table.
-                    compaction_query = f'CALL local.system.rewrite_data_files(table => \'{db}.{table_name}\')'
+                    compaction_query = f'CALL {self.catalog}.system.rewrite_data_files(table => \'{db}.{table_name}\')'
                     self.spark.sql(compaction_query)
             logging.info("Compaction ended")
+        except Exception as error:
+            logging.error("Error message:", error)
+
+    async def remove_orphan(self):
+        logging.info("In remove Orphans")
+        try:
+            # get database name list
+            databases = self.spark.catalog.listDatabases()
+            db_list = [db.name for db in databases]
+            for db in db_list:
+                # get table names of every db
+                tables = self.spark.catalog.listTables(db)
+                table_list = [table.name for table in tables]
+                for table_name in table_list:
+                    # 2. Call remove_orphan_files  query on the table.
+                    delete_orphan_files_query = f'CALL {self.catalog}.system.remove_orphan_files(table => \'{db}.{table_name}\');'
+                    self.spark.sql(delete_orphan_files_query)
+            logging.info("Remove orphans ended")
         except Exception as error:
             logging.error("Error message:", error)
