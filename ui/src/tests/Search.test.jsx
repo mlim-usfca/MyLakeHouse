817fedb4aba87789ba9fb9528770d15f7b1912f1
jingjingcai <jcai28@usfca.edu>
2024-05-02 15:17:10 -0700

    test should await for res

--- ui/src/tests/Search.test.jsx
+++ ui/src/tests/Search.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react';
 import {App} from '@/App.jsx'
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
@@ -11,12 +11,14 @@ const mock = new MockAdapter(axios);
 
 mock.onGet(`${import.meta.env.VITE_HOST}/dashboard/list-databases`).reply(200, {db_list: ["toyDb"] });
 
-test('renders search database with db_list', () => {
+test('renders search database with db_list', async() => {
     render(<MessageProvider>
         <MemoryRouter initialEntries={['/']}>
             <SearchDB/>
         </MemoryRouter>
     </MessageProvider>)
-    const element = screen.getByText('toyDb')
-    expect(element).toBeDefined()
+    await waitFor(() => {
+        const element = screen.getByText('toyDb');
+        expect(element).toBeDefined();
+    });
 })
