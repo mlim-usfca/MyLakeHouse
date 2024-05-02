2f73d8818157c029d5f20377812fa4c9243638a4
jingjingcai <jcai28@usfca.edu>
2024-05-02 16:18:56 -0700

    revert the change

--- ui/src/tests/App.test.jsx
+++ ui/src/tests/App.test.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { MemoryRouter, Route } from 'react-router-dom';
 import {MessageProvider} from "@/contexts/message.jsx";
-import { SearchDB } from '@/components/pages/search/SearchDB';
 
 
 const mock = new MockAdapter(axios);
