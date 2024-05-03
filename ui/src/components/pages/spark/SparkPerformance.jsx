9c7bb7d39821892ab7e3cded7b25f94bae8c33b9
Yin Hu (Eva) <yinhueva@gmail.com>
2024-05-03 10:06:39 -0700

    changed src url in iframe

--- ui/src/components/pages/spark/SparkPerformance.jsx
+++ ui/src/components/pages/spark/SparkPerformance.jsx
@@ -5,7 +5,7 @@ export const SparkPerformance = () => {
     return (
         <Box sx={{ width: '100%', height: '100%' }}>
             <iframe
-                src="http://localhost:3001/public-dashboards/e3259924ddb34edabc96258a890e1ab5"
+                src="http://localhost:3001/dashboard/new?orgId=1&from=1714729957497&to=1714751557497&viewPanel=1"
                 frameBorder="0"
                 width="100%"
                 height="100%"
