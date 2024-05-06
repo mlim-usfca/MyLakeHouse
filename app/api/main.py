cfd50e2db7c898b15216cda05b1d4d13ae5af662
Seth Villa <sethvilla13@gmail.com>
2024-05-06 01:29:31 -0700

    update cors

--- app/api/main.py
+++ app/api/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "http://caspian.sethvilla.com/",
+    "http://caspian.sethvilla.com",
     "http://sethvilla.com",
     os.environ.get("HOST", "http://localhost:3000")
 ]
