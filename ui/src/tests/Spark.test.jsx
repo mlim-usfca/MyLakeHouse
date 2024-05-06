5ddc279c66a1d6e0b77fc9b6016a1b3ea3039061
Yin Hu (Eva) <yinhueva@gmail.com>
2024-05-06 10:23:57 -0700

    fix a bug occurred in CI/CD process

--- ui/src/tests/Spark.test.jsx
+++ ui/src/tests/Spark.test.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { SparkPerformance } from '@/components/pages/spark/SparkPerformance.jsx';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import {MessageProvider} from "@/contexts/message.jsx";
+
+
+const mock = new MockAdapter(axios);
+
+mock.onGet(`${import.meta.env.VITE_HOST}/test`).reply(200, { data: 'Mocked response' });
 
 test('renders the Grafana dashboard', () => {
-    render(
+    render(<MessageProvider>
         <MemoryRouter initialEntries={['/spark-performance']}>
-            <Routes>
-                <Route path="/spark-performance" element={<SparkPerformance />} />
-            </Routes>
+            <SparkPerformance />
         </MemoryRouter>
-    );
+    </MessageProvider>)
 
     const iframe = screen.getByRole('iframe');
     expect(iframe).toBeInTheDocument();
