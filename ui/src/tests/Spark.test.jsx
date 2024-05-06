636b65fce4e633952f71bbcac50ac6874d07716b
Yin Hu (Eva) <yinhueva@gmail.com>
2024-05-05 23:52:36 -0700

    fix a bug occurred in CI/CD process.

--- ui/src/tests/Spark.test.jsx
+++ ui/src/tests/Spark.test.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import { SparkPerformance } from '@/components/pages/spark/SparkPerformance.jsx';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { SparkPerformance } from './SparkPerformance';
 
-describe('SparkPerformance', () => {
-    test('renders the Grafana dashboard iframe', () => {
-        render(<SparkPerformance />);
+test('renders the Grafana dashboard iframe', () => {
+    render(
+        <MemoryRouter initialEntries={['/spark-performance']}>
+            <Route path="/spark-performance" element={<SparkPerformance />} />
+        </MemoryRouter>
+    );
 
-        const iframe = screen.getByRole('iframe');
-        expect(iframe).toBeInTheDocument();
-        expect(iframe).toHaveAttribute(
-            'src',
-            `${process.env.VITE_HOST}:3001/d/ddkn6773qomwwf/sql-query-duration?orgId=1&from=1714782930603&to=1714783830603&viewPanel=1`
-        );
-    });
+    const iframe = screen.getByRole('iframe');
+    expect(iframe).toBeInTheDocument();
+    expect(iframe).toHaveAttribute(
+        'src',
+        `${process.env.VITE_HOST}:3001/d/ddkn6773qomwwf/sql-query-duration?orgId=1&from=1714782930603&to=1714783830603&viewPanel=1`
+    );
 });
